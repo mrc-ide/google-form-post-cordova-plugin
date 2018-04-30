57fa17d1164bf5d9dc9eb0bbce011313da853dec
Alex Hill <alex.hill@gmail.com>
2018-04-30 18:29:22 +0100

    inital commit

---
+++ src/android/GoogleFormPost.java
@@ -0,0 +1,30 @@
+package org.imperial.dide;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class GoogleFormPost extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("submit")) {
+            String message = args.getString(0);
+            this.submit(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void submit(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
