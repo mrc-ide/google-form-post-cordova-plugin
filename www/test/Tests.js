2a0eab1e432092a29582993874f638d07fff5ed2
Alex Hill <alex.hill@gmail.com>
2018-05-09 18:11:33 +0100

    wip

---
+++ www/test/Tests.js
@@ -0,0 +1,11 @@
+import {expect} from "chai";
+import {submit} from "../googleFormPost"
+
+describe('js interface', function () {
+
+    it('should call cordova exec', function () {
+        submit()
+
+    });
+
+});
